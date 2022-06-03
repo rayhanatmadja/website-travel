0cff0819c7ed91381a45bbb7dbdd571f77622dc7
Ryhn162004 <87838000+Ryhn162004@users.noreply.github.com>
2022-06-03 11:30:52 +0700

    logoTxt

---
+++ src/parts/LogoTxt.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Button from "../components/Button/Button";
+
+const LogoTxt = () => {
+  return (
+    <Button className="btn-logo-txt" href="" type="link">
+      Fun <span className="text-dark-blue">Cation.</span>
+    </Button>
+  );
+};
+
+export default LogoTxt;
